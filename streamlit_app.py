c2bf9f82083720076d64e22c3e5ae5b990bc60c0
Ashwary Mahendra Dhakate <160899030+AshwaryDhakate@users.noreply.github.com>
2026-09-21 13:57:16 +0530

    Change Snowflake connection string to empty
    Updated Snowflake connection string to an empty value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.write('The name on your smoothie will be: ', name_on_order)
 
 #from snowflake.snowpark.functions import col
 
-cnx = st.connection("snowflake")
+cnx = st.connection(" ")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
