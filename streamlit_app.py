5b704b14148233db608bbd47f466d415f1649b33
Ashwary Mahendra Dhakate <160899030+AshwaryDhakate@users.noreply.github.com>
2026-09-21 13:58:22 +0530

    Update Snowflake connection string in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.write('The name on your smoothie will be: ', name_on_order)
 
 #from snowflake.snowpark.functions import col
 
-cnx = st.connection(" ")
+cnx = st.connection("Snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
